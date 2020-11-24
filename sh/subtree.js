54750560af059a048c3a8ac54d93dc11f30f0bc3
mashaoliang <mashaoliang@rouchi.com>
2020-11-24 11:34:16 +0800

    feat: 添加指定分支功能

--- sh/subtree.js
+++ sh/subtree.js
@@ -1,9 +1,13 @@
 const shell = require("shelljs");
+const { exit } = require("yargs");
 const argv = require('yargs').argv;
+const branch = argv._[0] || 'main';
+// npm run pushSub -- test
+console.log(argv._[0]); // test
 
 const repositoryUrl = 'https://github.com/salen-ma/sub-lib.git'
-const pullExec = `git subtree pull --prefix sub-lib ${repositoryUrl} main --squash`
-const pushExec = `git subtree push --prefix sub-lib ${repositoryUrl} main`
+const pullExec = `git subtree pull --prefix sub-lib ${repositoryUrl} ${branch} --squash`
+const pushExec = `git subtree push --prefix sub-lib ${repositoryUrl} ${branch}`
 if (argv.type === 'pull') {
   console.log(pullExec)
   shell.exec(pullExec)
